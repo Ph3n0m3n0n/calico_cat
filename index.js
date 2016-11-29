218adfaa4ed47b4445ca459a0a6ea00f26c3e309
Kantaris <kantarisinnovations.staff@gmail.com>
2016-11-28 19:13:04 -0600

    Added some code to the index.js file.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Genre =require('./models/genre');
 Book =require('./models/book');
 
 // Connect to Mongoose
-mongoose.connect('mongodb://localhost/bookstore');
+mongoose.connect('mongodb://localhost/calico_cat');
 var db = mongoose.connection;
 
 app.get('/', function(req, res){
